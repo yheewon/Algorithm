86eb5d074e7167043b3148733545aa2f5b7ea990
윤희원 <49652068+yheewon@users.noreply.github.com>
2020-12-29 21:01:34 +0900

    Create Baek_나이순정렬.py

---
+++ Baek_나이순정렬.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+array = []
+for _ in range(N):
+    data = input().split(' ')
+    array.append((int(data[0]),data[1]))
+
+array = sorted(array, key= lambda x: x[0])
+
+for i in array:
+    print(i[0], i[1])
+
