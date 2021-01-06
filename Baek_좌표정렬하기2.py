e386832d4ef5a70df0f31cc9d3e7ad1bdae1e26e
윤희원 <49652068+yheewon@users.noreply.github.com>
2021-01-06 22:24:33 +0900

    Update Baek_좌표정렬하기2.py

--- Baek_좌표정렬하기2.py
+++ Baek_좌표정렬하기2.py
@@ -6,7 +6,7 @@ for _ in range(N):
     a,b = map(int,input().split())
     array.append((a,b))
 
-array = sorted(array, key = lambda array: array[1])
+array.sort(key = lambda x: (x[1],x[0]))
 
 for i in array:
     print(i[0],i[1])
