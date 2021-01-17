116b5b6eb084276728515a87d2de78c3fb728c65
윤희원 <49652068+yheewon@users.noreply.github.com>
2021-01-17 10:11:07 +0900

    Update Baek_바이러스.py

--- Baek_바이러스.py
+++ Baek_바이러스.py
@@ -1,29 +1,40 @@
 from collections import deque
-
-def bfs(start,cnt,visited):
+def bfs(start,cnt):
     queue = deque([start])
-
     visited[start] = True
-    while queue:
-        v = queue.popleft()
-        cnt += 1
 
-        for i in range(1,C+1):
-            if not visited[i] and graph[v][i] == 1:
+    while queue:
+        n = queue.popleft()
+        cnt += 1
+        for i in range(1,computer+1):
+            if not visited[i] and graph[n][i] == 1:
                 queue.append(i)
                 visited[i] = True
     return cnt-1
 
-C = int(input())
-N = int(input())
+def dfs(v):
+    visited[v] = True
+    global cnt
+    cnt += 1
+    for i in range(1,computer+1):
+        if not visited[i] and graph[v][i] == 1:
+            dfs(i)
 
-graph = [[0]*(C+1) for i in range(C+1)]
-visited = [False] * (C+1)
+    return cnt-1
+
+    
+
+
+computer = int(input())
+N = int(input())
+graph = [[0]*(computer+1) for _ in range(computer+1)]
+visited = [False] * (computer+1)
 
 for _ in range(N):
     a,b = map(int,input().split())
     graph[a][b] = graph[b][a] = 1
 
 cnt = 0
-
-print(bfs(1,cnt,visited))
+# print(bfs(1,cnt))
+dfs(1)
+print(cnt-1)
