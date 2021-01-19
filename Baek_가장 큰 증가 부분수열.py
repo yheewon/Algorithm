fc4b952f3a241c3d1eccb4e57b4558a6d2eaf9a3
윤희원 <49652068+yheewon@users.noreply.github.com>
2021-01-19 17:33:44 +0900

    Create Baek_가장 큰 증가 부분수열.py

---
+++ Baek_가장 큰 증가 부분수열.py
@@ -0,0 +1,12 @@
+import copy
+N = int(input())
+
+A = list(map(int,input().split()))
+DP = copy.deepcopy(A)
+
+for i in range(1,N):
+    for j in range(i):
+        if A[i] > A[j]:
+            DP[i] = max(A[i] + DP[j] , DP[i])
+
+print(max(DP))
