638cc947a505247e97a1a348f4a12a6dd8076250
윤희원 <49652068+yheewon@users.noreply.github.com>
2022-06-26 22:29:57 +0900

    Update 프로그래머스_체육복.py
    #그리디알고리즘#리스트#정렬

--- 프로그래머스_체육복.py
+++ 프로그래머스_체육복.py
@@ -1,11 +1,11 @@
 def solution(n, lost, reserve):
-    _reserve = [r for r in reserve if r not in lost]
-    _lost = [l for l in lost if l not in reserve]
-    for r in _reserve:
-        f = r - 1
-        b = r + 1
-        if f in _lost:
-            _lost.remove(f)
-        elif b in _lost:
-            _lost.remove(b)
-    return n - len(_lost)
+    res_student = sorted([x for x in reserve if x not in lost])
+    lost_student = sorted([x for x in lost if x not in reserve])
+    
+    for i in res_student :
+        if i-1 in lost_student : 
+            lost_student.remove(i-1)
+        elif i+1 in lost_student : 
+            lost_student.remove(i+1)
+        
+    return n - len(lost_student)
